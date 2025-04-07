b983893ab31f7e0d7ffab394247c21d0000a89b0
Avi Boru <avi@aviboru.com>
2025-04-06 22:37:36 -0400

    cluster config 4

--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -3,6 +3,11 @@ variable "cluster_name" {
   type        = string
 }
 
+variable "environment" {
+  description = "Environment"
+  type        = string
+}
+
 variable "subnet_ids" {
   description = "List of subnet IDs for the EKS cluster"
   type        = list(string)
